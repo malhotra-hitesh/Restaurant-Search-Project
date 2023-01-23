c0b36ab550dec5238e976230bc3666144e527e21
Vjosa <vjossa_h@live.com>
2023-01-23 11:39:46 +0100

    Created a react app, cleaned the project, added dependencies like axios, redux and styled components

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+         Initial fronted you can start now working on the components
+        </p>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
