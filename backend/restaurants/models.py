f4c2233de861e31bb85c4ae643e8e487f8f16d24
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2023-01-25 15:17:01 +0100

    search working, home not

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from luna import settings
 
 CATEGORIES = (
-        ('Fast food', 'FAST_FOOD'),
-        ('Casual dining', 'CASUAL_DINING'),
-        ('Fast Casual', 'FAST_CASUAL'),
-        ('Contemporary', 'CONTEMPORARY'),
-        ('Café', 'CAFE'),
-        ('Pizzeria', 'PIZZERIA')
+        (1, 'FAST_FOOD'),
+        (2, 'CASUAL_DINING'),
+        (3, 'FAST_CASUAL'),
+        (4, 'CONTEMPORARY'),
+        (5, 'CAFE'),
+        (6, 'PIZZERIA')
     )
 
 PRICES = (
@@ -23,17 +23,18 @@ class Restaurant(models.Model):
     country = models.TextField()
     city = models.TextField()
     street = models.TextField()
-    zip = models.IntegerField(blank=True, null=True)
+    zip = models.CharField(max_length=20, blank=True, null=True)
     website = models.TextField(blank=True, null=True)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
     opening_hours = models.TextField()
-    price = models.IntegerField(choices=PRICES, default=1)
+    price = models.IntegerField(choices=PRICES, blank=True, null=True)
     image = models.ImageField(max_length=255, blank=True)
-    email = models.EmailField(unique=True)
     owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    # rating_average = models.IntegerField(blank=True, editable=False)
 
-    # def compute_rating_average(self):
-    #     reviews = Restaurant.objects.filter(reviews_restaurant=self.id)
+    # def save(self, *args, **kwargs):
+    #     reviews = Restaurant.objects.filter(reviews__restaurant__id=self.id)
     #     sum = 0
     #     count = 0
     #     for review in reviews:
@@ -44,12 +45,11 @@ class Restaurant(models.Model):
     #             count = count + 1
     #
     #     if sum == 0:
-    #         return 'unkown'
+    #         self.rating_average = 'unkown'
     #     else:
-    #         return sum / count
-    #
-    # rating_average = models.IntegerField(blank=True, default=compute_rating_average())
-    # rating_average = models.IntegerField(blank=True, default=0)
+    #         self.rating_average = sum / count
+    #     super(Restaurant, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return f'Restaurant {self.id}: {self.name}'
