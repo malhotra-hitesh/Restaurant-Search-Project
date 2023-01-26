1ec7f6111ef1e474aaad893a5d79e4d2f0a9db4a
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2023-01-26 15:24:27 +0100

    fix errors after the merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,25 @@
 import {Route, Routes} from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/Sign-up/SignUp';
 import Congratulations from './pages/Congratulations/Congratulations';
 import Verification from './pages/Verification/Verification';
 import SignIn from './pages/Sign-in/SignIn';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Search from "./pages/Search"
 import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <>
             <div className="App">
-            <Header/>
-            <Routes>
-                <Route path="/search" element={<Search/>}/>
-                <Route path = "/home" element={<HomePage/>}/>
-                <Route path='/' element={<SignIn />} />
-               <Route path='/sign-up' element={<SignUp />} />
-               <Route path='/congratulations' element={<Congratulations />} />
-                <Route path='/verification' element={<Verification />} />
-            </Routes>
-            <Footer/>
-                </div>
+                <Header/>
+                <Routes>
+                    <Route path='/' element={<HomePage/>} />
+                    <Route path='/sign-up' element={<SignUp />} />
+                    <Route path='/congratulations' element={<Congratulations />} />
+                    <Route path='/verification' element={<Verification />} />
+                </Routes>
+                <Footer/>
+            </div>
         </>
     );
 
