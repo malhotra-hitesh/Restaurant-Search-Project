46c0c86c2fe601dd6cf9a228d9f7b8a47fa1eebb
Vjosa <vjossa_h@live.com>
2023-01-27 14:51:34 +0100

    Fixed styling for the user profile

--- frontend/src/pages/UserProfile/UserProfile.js
+++ frontend/src/pages/UserProfile/UserProfile.js
@@ -4,36 +4,39 @@ import  edit from '../../assets/edit.svg';
 import React, {useEffect, useState} from 'react';
 import  comment from '../../assets/comment.svg';
 import  restaurant from '../../assets/restaurant.svg';
-import {Vjosa, UserProfileStyled, Nina, Sara} from "./UserProfile.styled";
+import { UserProfileStyled, Nina, Sara, AboutProfile} from "./UserProfile.styled";
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUser } from '../../features/slice/userSlice'
+import {useNavigate} from "react-router-dom";
 
 
 const UserProfile = () => {
 const [data, setData] = useState({
         type: 'reviews'
     });
-
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const userData = useSelector(store => store.userProfile.userProfileSlice)
-     // aveer.harland@moabuild.com 123
     useEffect(() => {
-     dispatch(fetchUser())
+        dispatch(fetchUser())
 
     },[])
        const handleClick = e => {
-        console.log("clickeedddd")
         setData(prevState => {
             return {...prevState,
                     [e.target.name]: e.target.value}
         });
 
     };
+    const dataJoined = userData?.date_joined;
+    let today = new Date(dataJoined)
+let date = today.getDate() + '-' + parseInt(today.getMonth() + 1) + '-' + today.getFullYear()
+console.log(date)
   return (<>
     <UserProfileStyled>
-  <div>
-    <Vjosa>
+
     <Nina>
+
       <span><img src={profile}/></span>
          <span>{userData?.username}'s Profile</span>
       <Nina>
@@ -46,31 +49,54 @@ const [data, setData] = useState({
    <Nina>
       <span>{userData?.username}</span>
       <span>{userData?.location}</span>
-      <span>Reviews</span>
+      <span>{userData?.reviews?.length} reviews</span>
       <span>Comments</span>
     {/*  Conditional Rendering */}
        {data.type === 'reviews' ?
                         <Sara>
-                               <span>REVIEWS</span>
+                               <p>REVIEWS</p>
+                            <div>
+                                <span>Läderach Chocolatier Suisse</span>
+                                <p>This location at the Bahnhofstrasse is quite friendly and easily located across the street from the train station. The people there seem to be quite good and helpful in their service. </p>
+                            </div>
+                            <div>
+                                <span>Läderach Chocolatier Suisse</span>
+                                <p>This location at the Bahnhofstrasse is quite friendly and easily located across the street from the train station. The people there seem to be quite good and helpful in their service. </p>
+                            </div>
+
 
 
                         </Sara> : null }
-        {data.type === 'comments' ? <Sara>Comments </Sara> : null }
-        {data.type === 'restaurants' ? <Sara>Restaurants </Sara> : null }
-        {data.type === 'edit' ? <Sara>Edit </Sara> : null }
+        {data.type === 'comments' ? <Sara> <p>COMMENTS</p>
+                            <div>
+                                <span>Review 1</span>
+                                <p>This is horrible!</p>
+                            </div>
+                            <div>
+                                <span>Review 2</span>
+                                <p>That's a good idea! </p>
+                            </div></Sara> : null }
+        {data.type === 'restaurants' ? <Sara><p>RESTAURANTS</p>
+         <div>
+                                <span>Laurentio Gelato Store</span>
+                                <p>Lorem ipsum dolor sit amet, et legere populo quodsi sea. Nec quodsi albucius eu, simul soleat accusata te sea. Vix maluisset sententiae et, eam an salutatus consectetuer, ludus partiendo te ius. Vivendum convenire pro in. Cum impedit honestatis eu.</p>
+                            </div>
+                            <button type="button" id="signup" onClick={() => navigate("/restaurants")}>Create Restaurant</button>
+                        </Sara> : null }
+        {data.type === 'edit' ? <Sara>
+            Edit Profile</Sara> : null }
     </Nina>
-    <Nina>
+    <AboutProfile>
+        <span>About {userData?.username}</span>
       <label>Location</label>
-        <span>{userData?.location}</span>
+        <p>{userData?.location}</p>
        <label>Luna member since</label>
-        <span>{userData?.date_joined}</span>
+        <p>{date}</p>
         <label>Things I love</label>
-        <span>{userData?.things}</span>
+        <p>{userData?.things_I_love}</p>
         <label>Description</label>
-        <span>{userData?.description}</span>
-    </Nina>
-      </Vjosa>
-  </div>
+        <p>{userData?.description}</p>
+    </AboutProfile>
 
       </UserProfileStyled>
   </>)
--- frontend/src/pages/UserProfile/UserProfile.styled.js
+++ frontend/src/pages/UserProfile/UserProfile.styled.js
@@ -3,23 +3,18 @@ import styled from 'styled-components'
 export const UserProfileStyled = styled.div`
 
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  span {
-    img {
-      width: 100px;
-      height: 100px;
+    background: #F2F2F2;
+  //background-image: '../../assets/background.png';  
+    margin-top: 25px;
+    padding-bottom: 115px;
+    justify-content: space-around;
+      span {
+        img {
+          width: 100px;
+          height: 100px;
+        }
     }
-  }
 `
-export const Vjosa = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #ebebeb;
-  width: 100%;
-
-`;
 
 export const Nina = styled.div`
 display: grid;
@@ -36,12 +31,92 @@ display: grid;
   button:focus {
     background: rgba(0, 0, 0, 0.0837013);
   }
+ 
 `
 
+export const AboutProfile = styled.div`
+display: grid;
+  max-width: 250px;
+  max-height: 350px;
+  padding-top: 75px;
+  
+  span {
+    font-size: 20px;
+    font-family: Roboto;
+    font-weight: 700;
+    line-height: 23.4px;
+    padding-bottom: 15px;
+  }
+  label {
+    font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 23px;
+color: #000000;
+  }
+  
+  p {
+    font-family: 'Roboto';
+font-style: normal;
+font-weight: 300;
+font-size: 20px;
+line-height: 23px;
+color: #000000;
+    padding-bottom: 25px;
+  }
+`
 export const Sara = styled.div`
-
-  width: 450px;
+  background: #FFFFFF;
+  width: 619px;
   height: 380px;
   padding-top: 15px;
   border: 1px solid #EBEBEB;
+  div {
+     margin-top: 35px;
+    border-bottom:1px solid #EBEBEB ;
+    
+     p {
+       padding-top: 15px;
+       font-family: Roboto;
+        font-weight:300 ;
+       font-size: 16px;
+       color: #303030;
+     }
+    span {
+      font-size: 20px;
+      font-weight: 400;
+      font-family: Roboto;
+      color: #4C4C4C;
+    }
+  }
+  p {
+    font-family: Roboto;
+    font-weight: 700;
+    font-size: 20px;
+    color: #303030;
+  }
+  span {
+   
+  
+  }
+  button {
+    width: 200px;
+    height:57px;
+    background:#E47D31;
+    border-radius: 28px;
+    font-size: 20px;
+    font-family: Roboto;
+    font-style: normal;
+    color: white;
+    border: none;
+    cursor: pointer;
+    position: relative;
+    top: 40px;
+    left:190px;
+    font-weight:400;
+}
 `
+
+;
+
